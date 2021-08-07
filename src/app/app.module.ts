b22afcc276ef4d7b7fdc80d0c0ad41204605eed6
Ananthu.V.A <ananthu.va@elkanio.com>
2021-08-07 22:28:50 +0530

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
