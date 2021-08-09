e33acfdd4bb843e0d4fc56d67a49d88686a9500e
Ananthu.V.A <ananthu.va@elkanio.com>
2021-08-09 22:28:32 +0530

    show clss and random values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,17 @@ export class AppComponent {
   title = 'opzet-farm';
   public isCollapsed = false;
   public selectedIndex=1; //default
+  public humidity = this.getRandomNum(100,50); //default
+  public temarature =  this.getRandomNum(100,50); //default
+  public conductivity =  this.getRandomNum(100,50); //default
+
+  generateRandom() {
+    this.humidity  =  this.getRandomNum(100,50);
+    this.temarature  =  this.getRandomNum(100,50);
+    this.conductivity  =  this.getRandomNum(100,50);
+  }
+
+  getRandomNum(max,min){
+    return Math.floor(Math.random() *  (max - min) + min);
+  }
 }
