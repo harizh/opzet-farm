b8957a0b6ec91bcc4a9003912352bf006aaf0b1d
Ananthu.V.A <ananthu.va@elkanio.com>
2021-08-09 22:50:45 +0530

    text change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'opzet-farm';
   public isCollapsed = false;
-  public selectedIndex=1; //default
+  public selectedIndex = 1; //default
   public humidity = this.getRandomNum(100,50); //default
   public temarature =  this.getRandomNum(100,50); //default
   public conductivity =  this.getRandomNum(100,50); //default
+
+  public textDisplay = [
+  {heading: "Caution 1", description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit."},
+  {heading: "Caution 2", description: "Et harum quidem rerum facilis est et expedita distinctio adipisicing."},
+  {heading: "Caution 3", description: "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores."},
+  ];
 
   generateRandom() {
     this.humidity  =  this.getRandomNum(100,50);
